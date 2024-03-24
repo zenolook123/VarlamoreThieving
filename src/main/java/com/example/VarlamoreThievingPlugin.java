4ffbb973532d6eda57a9f4d53722386c7cfca3f9
zenolook123 <99941161+zenolook123@users.noreply.github.com>
2024-03-24 14:56:51 -0500

    naming changed

--- src/main/java/com/example/VarlamoreThievingPlugin.java
+++ src/main/java/com/example/VarlamoreThievingPlugin.java
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Varlamore Thieving"
 )
-public class ExamplePlugin extends Plugin
+public class VarlamoreThievingPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private VarlamoreThievingConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	VarlamoreThievingConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(VarlamoreThievingConfig.class);
 	}
 }
--- src/test/java/com/example/VarlamoreThieving.java
+++ src/test/java/com/example/VarlamoreThieving.java
@@ -3,11 +3,11 @@ package com.example;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class VarlamoreThieving
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(VarlamoreThievingPlugin.class);
 		RuneLite.main(args);
 	}
 }
